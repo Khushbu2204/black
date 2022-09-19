fb6c30d93ad7f993ea6d7873a6dc957a2d0c0597
Shivam Dhuware <shivamdhuwar011@gmail.com>
2022-09-19 23:08:40 +0530

    changes

--- src/views/Pages/Login.js
+++ src/views/Pages/Login.js
@@ -1,73 +1,144 @@
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { Input } from "reactstrap";
 
-import React from "react";
-import { NavLink, Link, useLocation } from "react-router-dom";
+const LoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .required()
+    .min(4, "Username must be 4 characters at minimum"),
+  password: Yup.string()
+   .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,20}$/,
+      //"Min 1 uppercase letter, Min 1 lowercase letter, Min 1 special character, Min 1 number, Min 8 characters, Max 20 characters."
+    )
+    .required(),
+});
 
-// reactstrap components
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  FormGroup,
-  Form,
-  Input,
-  Row,
-  Col
-} from "reactstrap";
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState();
+  
+  const handleSubmit = async (e) => {
+    alert("submit");
+    e.preventDefault();
+    const user = { username, password };
+   // console.log(username);
+    // send the username and password to the server
+   const response = await axios.post(
+      "http://localhost:8080/user/login",
+       user
+     );
+     // set the state of the user
+     setUser(response.data);
+     // store the user in localStorage
+     localStorage.setItem("user", response.data);
+     console.log(response.data);
+   };
 
-function UserLogin() {
+ /* const handleSubmit = ()=>{
+    alert("submit");
+    const user = { username, password };
+    localStorage.setItem("user", user);
+    console.log("uname:"+user)
+  }*/
   return (
-    <>
-      <div className="content">
-        <Row className="justify-content-md-center">
-          <Col md="4">
-            <Card>
-              <CardHeader>
-                <h5 className="title">Login page</h5>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-md-1" md="12">
-                      <FormGroup>
-                        <label>User name</label>
-                        <Input placeholder="User@1" type="text" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="12">
-                      <FormGroup>
-                        <label>Password</label>
-                        <Input placeholder="**********" type="password" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <Button className="btn-fill btn-block" color="primary" type="submit">
-                  Login
-                </Button><br/>
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="col-md-4">
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => {
+              console.log(values);
+              alert("Form is validated! Submitting the form...");
+            }}
+          >
+            {({ touched, errors, isSubmitting, values }) =>
+              !isSubmitting ? (
+                <div>
+                  <div className="row mb-5">
+                    <div className="col-lg-12 text-center">
+                      <h1 className="mt-5">Login Form</h1>
+                    </div>
+                  </div>
+                  <Form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="username">Username</label>
+                      <Input
+                        tag={Field}
+                        name="username"
+                        placeholder="Enter username"
+                        className={`mt-2 form-control
+						${touched.username && errors.username ? "is-invalid" : ""}`}
+                       // onChange={({ target }) => setUsername(target.value)}
+						onChange={e => setUsername(e.target.value)}
+                        value={username}
+                      />
 
-                <NavLink to="login" className="ml-5" >
-                       <span className="">
-                          Forget password
-                        </span> 
-                    </NavLink>
-                    <NavLink to="register" className="ml-5" >
-                       <span className="">
-                          Register
-                        </span> 
-                    </NavLink>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
+                      <ErrorMessage
+                        component="div"
+                        name="username"
+                        className="invalid-feedback"
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="password" className="mt-3">
+                        Password
+                      </label>
+                      <Input
+                        tag={Field}
+                        type="password"
+                        name="password"
+                        placeholder="Enter password"
+                        className={`mt-2 form-control
+						${touched.password && errors.password ? "is-invalid" : ""}`}
+                        //onChange={({ target }) => setPassword(target.value)}
+						onChange={e => setPassword(e.target.value)}
+                        value={password}
+                      />
+                      <ErrorMessage
+                        component="div"
+                        name="password"
+                        className="invalid-feedback"
+                      />
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block mt-4"
+                    >
+                      Login
+                    </button>
+                  </Form>
+                </div>
+              ) : (
+                <div>
+                  <h1 className="p-3 mt-5">Form Submitted</h1>
+
+                  <div className="alert alert-success mt-3">
+                    Thank for your connecting with us. Here's what we got from
+                    you !
+                  </div>
+                  <ul className="list-group">
+                    <li className="list-group-item">Email: {values.email}</li>
+                    <li className="list-group-item">
+                      Password: {values.password}
+                    </li>
+                  </ul>
+                </div>
+              )
+            }
+          </Formik>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default UserLogin;
+export default Login;
+
