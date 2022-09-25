60ba71934b77e13efe63c55ad317f1f7ac1f84cb
Khushbu2204 <khushboo.22048@gmail.com>
2022-09-26 01:25:33 +0530

    correction in login page

--- src/views/Pages/Login.js
+++ src/views/Pages/Login.js
@@ -1,142 +1,144 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import { useFormik } from "formik";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
-import { Input } from "reactstrap";
-
-const LoginSchema = Yup.object().shape({
-  username: Yup.string()
-    .required()
-    .min(4, "Username must be 4 characters at minimum"),
-  password: Yup.string()
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,20}$/,
-      //"Min 1 uppercase letter, Min 1 lowercase letter, Min 1 special character, Min 1 number, Min 8 characters, Max 20 characters."
-    )
-    .required(),
-});
+import {
+  Input,
+  Form,
+  Label,
+  FormGroup,
+  FormFeedback,
+  Button,
+} from "reactstrap";
 
 function Login(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState();
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [user, setUser] = useState();
+  // formik validations
+  const validate = (values) => {
+    const errors = {};
 
-  const handleSubmit = async (e) => {
-    alert("submit");
-    e.preventDefault();
-    const user = { username, password };
-    localStorage.setItem("user", user);
-    let logedIn = localStorage.getItem("user");
-    console.log(logedIn);
-    
-    console.log(user);
-    if(logedIn){
-      props.history.push("/admin");
+    if (!values.username) {
+      errors.username = "Please choose a unique and valid username.";
+    } else if (values.username.length > 50) {
+      errors.username = "Must be 10 characters or less";
     }
-    // const response = await axios.post(
-    //   "http://localhost:8080/login",
-    //   user
-    // );
-    // setUser(response.data);
-    // // localStorage.setItem("user", response.data);
-    // console.log(response.data);
+
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,20}$/i.test(
+        values.password
+      )
+    ) {
+      errors.password = "Invalid password";
+    }
+
+    return errors;
   };
+
+  // const handleSubmit = async (e) => {
+  //   alert("submit");
+  //   e.preventDefault();
+  //   const user = { username, password };
+  //   if (username && password) {
+  //     localStorage.setItem("user", user);
+  //     let logedIn = localStorage.getItem("user");
+  //     console.log(logedIn);
+  //     console.log(user);
+  //     if (logedIn) {
+  //       props.history.push("/admin");
+  //     }
+  //   }
+
+  //   // const response = await axios.post(
+  //   //   "http://localhost:8080/login",
+  //   //   user
+  //   // );
+  //   // setUser(response.data);
+  //   // // localStorage.setItem("user", response.data);
+  //   // console.log(response.data);
+  // };
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      localStorage.setItem("user", values);
+      props.history.push("/admin");
+      // alert(JSON.stringify(values, null, 2));
+      // if (formik.u && password) {
+      //       localStorage.setItem("user", user);
+      //       let logedIn = localStorage.getItem("user");
+      //       console.log(logedIn);
+      //       console.log(user);
+      //       if (logedIn) {
+      //         props.history.push("/admin");
+      //       }
+      //     }
+    },
+  });
 
   return (
     <div className="container">
       <div className="row justify-content-md-center">
         <div className="col-md-4">
-          <Formik
-            initialValues={{ username: "", password: "" }}
-            validationSchema={LoginSchema}
-            onSubmit={(values) => {
-              console.log(values);
-              alert("Form is validated! Submitting the form...");
-            }}
-          >
-            {({ touched, errors, isSubmitting, values }) =>
-              !isSubmitting ? (
-                <div>
-                  <div className="row mb-5">
-                    <div className="col-lg-12 text-center">
-                      <h1 className="mt-5">Login Form</h1>
-                    </div>
-                  </div>
-                  <Form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                      <label htmlFor="username">Username</label>
-                      <Input
-                        tag={Field}
-                        name="username"
-                        placeholder="Enter username"
-                        className={`mt-2 form-control
-						${touched.username && errors.username ? "is-invalid" : ""}`}
-                        // onChange={({ target }) => setUsername(target.value)}
-                        onChange={e => setUsername(e.target.value)}
-                        value={username}
-                      />
+          <div>
+            <div className="row mb-5">
+              <div className="col-lg-12 text-center">
+                <h1 className="mt-5">Login Form</h1>
+              </div>
+            </div>
+            <Form noValidate onSubmit={formik.handleSubmit}>
+              <FormGroup>
+                <Label for="userName">User Name</Label>
+                <Input
+                  invalid={formik.errors.username}
+                  id="username"
+                  name="username"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                />
+                <FormFeedback>Username is required</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Passwords</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  invalid={formik.errors.password}
+                />
+                <FormFeedback>Password is required</FormFeedback>
+              </FormGroup>
+              
+              <button type="submit" className="btn btn-primary btn-block mt-4">
+                Login
+              </button>
 
-                      <ErrorMessage
-                        component="div"
-                        name="username"
-                        className="invalid-feedback"
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="password" className="mt-3">
-                        Password
-                      </label>
-                      <Input
-                        tag={Field}
-                        type="password"
-                        name="password"
-                        placeholder="Enter password"
-                        className={`mt-2 form-control
-						${touched.password && errors.password ? "is-invalid" : ""}`}
-                        //onChange={({ target }) => setPassword(target.value)}
-                        onChange={e => setPassword(e.target.value)}
-                        value={password}
-                      />
-                      <ErrorMessage
-                        component="div"
-                        name="password"
-                        className="invalid-feedback"
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="btn btn-primary btn-block mt-4"
-                    >
-                      Login
-                    </button>
-                    <div className="row mt-3">
-                      <div className="col-6"> <Link to="register">Register</Link>
-                      </div>
-                      <div className="col-6 mr-0"> <p>Forgot Password</p></div>
-                    </div>
-                  </Form>
+              <div className="row mt-3">
+                <div className="col-6">
+                  {" "}
+                  <Link to="register" className="text-success">Register</Link>
                 </div>
-              ) : (
-                <div>
-                  <h1 className="p-3 mt-5">Form Submitted</h1>
-
-                  <div className="alert alert-success mt-3">
-                    Thank for your connecting with us. Here's what we got from
-                    you !
-                  </div>
-                  <ul className="list-group">
-                    <li className="list-group-item">Email: {values.email}</li>
-                    <li className="list-group-item">
-                      Password: {values.password}
-                    </li>
-                  </ul>
+                <div className="col-6 mr-0">
+                  {" "}
+                  <p>Forgot Password</p>
                 </div>
-              )
-            }
-          </Formik>
+              </div>
+            </Form>
+            
+          </div>
         </div>
       </div>
     </div>
@@ -144,4 +146,3 @@ function Login(props) {
 }
 
 export default Login;
-
